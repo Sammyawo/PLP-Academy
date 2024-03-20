93326a777a09716add27e69b0744364caf0250dc
samuel <awosamuel1@gmail.com>
2024-03-20 18:16:19 +0100

    This is it

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What's your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+message = 'Hello ' + name + '!, you are ' + age + ' years old and you live at ' + location +'.'
+print(message)
